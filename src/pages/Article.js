0ee3af6817524efb0affac777be0116b5a2aca81
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-12-08 22:52:17 +0800

    Update Article.js

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -8,7 +8,7 @@ const Article = () => {
 
   useEffect(() => {
       axios
-          .get('https://reactserver-production.up.railway.app/')
+          .get('https://reactproject3server-production.up.railway.app/')
           .then(res => {
               console.log(res)
           setPosts(res.data)
